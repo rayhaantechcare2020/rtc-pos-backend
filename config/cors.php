b3cba4ae88ed8b629e58506b33b596d6ed5d3aa6
rayhaantechcare2020 <rayhaantechcare2020@gmail.co>
2026-04-08 13:29:03 +0100

    modified config/cors

--- config/cors.php
+++ config/cors.php
@@ -8,18 +8,19 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'broadcasting/auth'],
 
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:8000/api/categories',
-        'https://rtc-pos-backend-production.up.railway.app/api',
         'https://rtcpos.netlify.app',
-        'http://localhost:5173',  // Add your Vite dev server
+        'https://www.rtcpos.netlify.app',
+        'https://localhost:8000/api/categories',
+        'https://www.rtc-pos-backend-production.up.railway.app/api',
+        'https://localhost:5173',  // Add your Vite dev server
         'http://localhost:3000',   // Keep existing
         'http://127.0.0.1:5173',
-        'http://127.0.0.1:8000'
+        'https://127.0.0.1:8000'
     ],
 
     'allowed_origins_patterns' => [],
